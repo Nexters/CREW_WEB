e2e6949b5eb8974eaec0f45bcade028a1ad70b8b
nayunhwan <nayunhwan.dev@gmail.com>
2019-01-04 14:59:40 +0900

    env: Setting Redux Saga

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createLogger } from "redux-logger";
+import createSagaMiddleware from "redux-saga";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import rootSaga from "./sagas/rootSaga";
+import rootReducer from "./reducers/rootReducer";
+
+import * as serviceWorker from "./serviceWorker";
+import App from "./App";
+import "./index.css";
+
+const sagaMiddleWare = createSagaMiddleware();
+const logger = createLogger();
+
+const middlewares = [];
+middlewares.push(sagaMiddleWare);
+if (process.env.NODE_ENV !== "production") {
+  middlewares.push(logger);
+}
+
+const store = createStore(rootReducer, applyMiddleware(...middlewares));
+sagaMiddleWare.run(rootSaga);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
---
+++ src/reducers/rootReducer.ts
@@ -0,0 +1,7 @@
+import { combineReducers } from "redux";
+
+export interface AppState {}
+
+const rootReducer = combineReducers({});
+
+export default rootReducer;
