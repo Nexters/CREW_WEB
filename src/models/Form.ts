72353d4e4cdf0dbd68efc0f2a634bbaf052d1f40
Yunhwan Na <nayunhwan.dev@gmail.com>
2019-01-13 19:18:47 +0900

    model: Define Form Models

---
+++ src/models/Form.ts
@@ -0,0 +1,33 @@
+export enum QuestionType {
+  Singline,
+  Multiline,
+  Select
+}
+
+export interface Question {
+  type: QuestionType;
+  title: string;
+  placeholder: string;
+  isRequired: boolean;
+}
+
+export interface SinglelineQuestion extends Question {
+  type: QuestionType.Singline;
+  value: string;
+}
+
+export interface MultilineQuestion extends Question {
+  type: QuestionType.Multiline;
+  value: string;
+}
+
+export interface SelectQuestion extends Question {
+  type: QuestionType.Select;
+  options: SelectQuestionOption[];
+  value: string;
+}
+
+export interface SelectQuestionOption {
+  key: string;
+  value: string;
+}
