770bb6b10016f8e8eb4b673d3e19ed319a65742a
Yunhwan Na <nayunhwan.dev@gmail.com>
2019-01-29 23:52:12 +0900

    views: Update ApplicantView

--- src/views/ApplicantView/index.tsx
+++ src/views/ApplicantView/index.tsx
@@ -1,6 +1,6 @@
 import React, { PureComponent } from "react";
 
-import { ApplicantItem } from "components";
+import { ApplicantItem, Paper } from "components";
 import { Applicant } from "models/Applicant";
 import MockedApplicants from "mocks/Applicants";
 
@@ -17,15 +17,23 @@ class ApplicantView extends PureComponent<Props> {
   public render() {
     const { applicants } = this.props;
     return (
-      <div>
-        <Styled.ApplicantList>
-          {applicants.map((applicant) => (
-            <li key={applicant.id}>
-              <ApplicantItem applicant={applicant} />
-            </li>
-          ))}
-        </Styled.ApplicantList>
-      </div>
+      <Styled.Container>
+        <Styled.Title>지원자 리스트</Styled.Title>
+        <div>
+          <Styled.Left>
+            <Paper style={{ padding: "27px 45px" }} width="620px">
+              <Styled.ApplicantList>
+                {applicants.map((applicant) => (
+                  <Styled.ApplicantItem key={applicant.id}>
+                    <ApplicantItem applicant={applicant} />
+                  </Styled.ApplicantItem>
+                ))}
+              </Styled.ApplicantList>
+            </Paper>
+          </Styled.Left>
+          <Styled.Right />
+        </div>
+      </Styled.Container>
     );
   }
 }
--- src/views/ApplicantView/styled.ts
+++ src/views/ApplicantView/styled.ts
@@ -1,7 +1,35 @@
 import styled from "styled-components";
+import { BG_GRAY, LIGHT_MAIN_COLOR } from "constants/colors";
+
+export const Container = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: ${BG_GRAY};
+  padding-top: 86px;
+`;
+
+export const Title = styled.h1`
+  font-size: 40px;
+  margin: 0;
+  padding: 0;
+  margin-bottom: 21px;
+`;
+
+export const Left = styled.div``;
+export const Right = styled.div``;
 
 export const ApplicantList = styled.ul`
   list-style: none;
   margin: 0;
   padding: 0;
+`;
+
+export const ApplicantItem = styled.li`
+  display: block;
+  padding: 10px 0;
+  border-bottom: 1px solid ${LIGHT_MAIN_COLOR};
+
+  :last-child {
+    border-bottom: none;
+  }
 `;
