2b36cbc4b7ca4fd04194ec521e2d208f20522182
Yunhwan Na <nayunhwan.dev@gmail.com>
2019-02-03 22:46:04 +0900

    component: Create Button Component

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,16 @@
 import React, { PureComponent, HTMLAttributes } from "react";
 import * as Styled from "./styled";
 
-export interface Props extends HTMLAttributes<HTMLButtonElement> {}
+export interface Props extends HTMLAttributes<HTMLButtonElement> {
+  primary: boolean;
+}
 interface State {}
 
 class Button extends PureComponent<Props, State> {
+  static defaultProps: Partial<Props> = {
+    primary: false
+  };
+
   public render() {
     return <Styled.Button {...this.props}>test</Styled.Button>;
   }
--- src/components/Button/styled.ts
+++ src/components/Button/styled.ts
@@ -1,5 +1,16 @@
 import styled from "styled-components";
 
+import { Props } from "./index";
+import { MAIN_COLOR } from "constants/colors";
+
 export const Button = styled.button`
-  background: red;
+  width: 100px;
+  padding: 10px 20px;
+  border: 1px solid ${MAIN_COLOR};
+  border-radius: 4px;
+  background: ${(props: Props) => (props.primary ? MAIN_COLOR : "white")};
+  color: ${(props: Props) => (props.primary ? "white" : MAIN_COLOR)};
+  font-size: 14px;
+  font-weight: bold;
+  cursor: pointer;
 `;
