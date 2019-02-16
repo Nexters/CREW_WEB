3afacfab586c4fb4c89923a865660f970febab88
Yunhwan Na <nayunhwan.dev@gmail.com>
2019-02-16 16:51:03 +0900

    wip

---
+++ src/reducers/formReducer.ts
@@ -0,0 +1,37 @@
+import { Action } from "models/Redux";
+import {
+  CREATE_FORM_ITEM,
+  UPDATE_FORM_ITEM,
+  REMOVE_FORM_ITEM
+} from "actionTypes/form";
+import { Question, QuestionType } from "models/Form";
+
+export interface State {
+  questions: Question[];
+}
+const initialState: State = {
+  questions: []
+};
+
+export const reducer = (
+  state: State = initialState,
+  action: Action<any>
+): State => {
+  switch (action.type) {
+    case CREATE_FORM_ITEM.REQUEST: {
+      const newItem: Question = {
+        type: QuestionType.SingleLine,
+        title: "",
+        placeholder: "",
+        isRequired: false
+      };
+      return {
+        ...state,
+        questions: [...state.questions, newItem]
+      };
+    }
+    default: {
+      return state;
+    }
+  }
+};
--- src/reducers/rootReducer.ts
+++ src/reducers/rootReducer.ts
@@ -1,7 +1,12 @@
 import { combineReducers } from "redux";
+import * as formReducer from "./formReducer";
 
-export interface AppState {}
+export interface AppState {
+  formReducer: formReducer.State;
+}
 
-const rootReducer = combineReducers({});
+const rootReducer = combineReducers({
+  formReducer: formReducer.reducer
+});
 
 export default rootReducer;
