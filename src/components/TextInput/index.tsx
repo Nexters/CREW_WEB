628708b4ca5f3a9d628cc65d23a01d00fa485ef3
HunJin Kim <ysg01129@naver.com>
2019-04-20 17:23:04 +0900

    [#40] 회원가입 유효성 검사
    1. TextInput 컴포넌트 개발
    2. InputType Enum 클래스 개발

---
+++ src/components/TextInput/index.tsx
@@ -0,0 +1,92 @@
+import React, { PureComponent, ChangeEvent, HTMLAttributes } from "react";
+import { InputType } from "src/models/Input";
+import { TextField } from "@material-ui/core";
+import * as Styled from "./styled";
+
+interface Props extends HTMLAttributes<HTMLInputElement> {
+  type: InputType;
+  label: string;
+  placeHolder?: string;
+}
+
+interface State {
+  textField: string;
+  isValidate: boolean;
+}
+
+class TextInput extends PureComponent<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = {
+      textField: "",
+      isValidate: true,
+    };
+  }
+
+  public render() {
+    return (
+      <TextField
+        label={this.props.label}
+        placeholder={this.props.placeholder}
+        onChange={this.handleChangeTextField}
+        margin="normal"
+      />
+    );
+  }
+
+  private handleChangeTextField = (e: ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+    const type = this.props.type;
+
+    switch (type) {
+      case InputType.STRING: {
+        this.setState({
+          textField: value,
+          isValidate: true,
+        });
+      }
+      case InputType.NUMBER: {
+        if (this.isValidateNumber(value)) {
+          this.setState({
+            textField: value,
+            isValidate: true,
+          });
+        }
+        break;
+      }
+      case InputType.EMAIL: {
+        this.setState({
+          textField: value,
+          isValidate: this.isValidateEmail(value),
+        });
+        break;
+      }
+      case InputType.PHONE: {
+        this.setState({
+          textField: value,
+          isValidate: this.isValidatePhone(value),
+        });
+        break;
+      }
+    }
+  };
+
+  private isValidateNumber(text: string): boolean {
+    const pattern = /^\d+$/;
+    return pattern.test(text);
+  }
+
+  private isValidateEmail(text: string): boolean {
+    const pattern = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*\.[a-zA-Z]{2,3}$/;
+    return pattern.test(text);
+  }
+
+  private isValidatePhone(text: string): boolean {
+    const patternA = /^\d{3}-\d{3,4}-\d{4}$/;
+    const patternB = /^\d{10,11}$/;
+
+    return patternA.test(text) || patternB.test(text);
+  }
+}
+
+export default TextInput;
